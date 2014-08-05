6b8cfb3b72182909643a16712edbce0aca009db5
Tom Sneyers <tom.sneyers@vito.be>
2014-08-05 09:38:52 +0200

    Added an empty generic server role.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,3 @@
-# Class: sas_roles
-#
-# This module manages sas_roles
-#
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
-#
-# Sample Usage:
-#
-class sas_roles {
+class roles {
 
 }
