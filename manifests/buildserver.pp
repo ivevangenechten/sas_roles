3976f4b391fd13c94120ef8fbc1268b92534a06f
Tom Sneyers <tom.sneyers@vito.be>
2014-09-17 08:38:39 +0200

    removedssh key management

--- manifests/buildserver.pp
+++ manifests/buildserver.pp
@@ -2,8 +2,4 @@ class roles::buildserver inherits roles::server {
   include profiles::java
   include profiles::jenkins
 
-  sshkeys::sshclient { 'jenkins': user => "jenkins", }
-  sshkeys::sshserver { 'aegir': user => "aegir", }
-  sshkeys::sshserver { 'jenkins': user => "jenkins", }
-
 }
--- manifests/hostmaster.pp
+++ manifests/hostmaster.pp
@@ -1,8 +1,4 @@
 class roles::hostmaster inherits roles::server {
   include profiles::aegir
 
-  sshkeys::sshclient { 'aegir': user => "aegir", }
-  sshkeys::sshserver { 'aegir': user => "aegir", }
-  sshkeys::sshserver { 'jenkins': user => "jenkins", }
-
 }
