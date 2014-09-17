943b8a8c583d7e2c599161296959f84e2edfd4cb
Tom Sneyers <tom.sneyers@vito.be>
2014-09-17 10:54:36 +0200

    beheeder sudoers rule

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,4 +1,8 @@
 class roles::server {
+  sudo::conf { 'beheerder':
+    content => 'beheerder ALL=(ALL) NOPASSWD: ALL',
+  }
+
   include sshkeys
   # include profiles::apt
   include profiles::nano
