43935cd003de4adaa9944bb6cc8ea064fdf7f151
Tom Sneyers <tom.sneyers@vito.be>
2014-09-17 11:26:16 +0200

    added aegir

--- manifests/buildserver.pp
+++ manifests/buildserver.pp
@@ -1,4 +1,9 @@
 class roles::buildserver inherits roles::server {
+
+  sudo::conf { 'aegir':
+    content => 'aegir ALL=(ALL) NOPASSWD: ALL',
+  }
+
   include profiles::java
   include profiles::jenkins
 
