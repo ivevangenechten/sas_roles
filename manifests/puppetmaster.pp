26d9d01c653826c365a95397c8c6f1c92604dae4
Tom Sneyers <tom.sneyers@vito.be>
2014-09-19 11:10:27 +0200

    added foreman sudoers settings

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -3,5 +3,11 @@ class roles::puppetmaster inherits roles::server {
   sudo::conf { 'jenkins':
     content => 'jenkins ALL=(ALL) NOPASSWD: /usr/local/bin/r10k, /usr/bin/svn',
   }
+  sudo::conf { 'foreman1':
+    content => 'Defaults:foreman-proxy !requiretty',
+  }
+  sudo::conf { 'foreman2':
+    content => 'foreman-proxy ALL = NOPASSWD: /usr/bin/puppetrun',
+  }
 
 }
