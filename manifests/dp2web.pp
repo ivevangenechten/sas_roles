e31a47e0a6c30d8829654e566758ecdd15af49a8
Tom Sneyers <tom.sneyers@vito.be>
2014-09-23 09:17:21 +0200

    added sudo config for aegir user on remote webservers

--- manifests/dp2web.pp
+++ manifests/dp2web.pp
@@ -1,5 +1,9 @@
 class roles::dp2web inherits roles::server {
 
+  sudo::conf { 'aegir':
+    content => 'aegir ALL=(ALL) NOPASSWD: /usr/bin/apache2ctl',
+  }
+
   include profiles::apache
   include profiles::php5
 
