7a1ac8a98fbe765118ed047d4dd87223b32c23e8
Tom Sneyers <tom.sneyers@vito.be>
2014-09-24 09:57:52 +0200

    made sbin instead of bin sudoable

--- manifests/dp2web.pp
+++ manifests/dp2web.pp
@@ -1,7 +1,7 @@
 class roles::dp2web inherits roles::server {
 
   sudo::conf { 'aegir':
-    content => 'aegir ALL=(ALL) NOPASSWD: /usr/bin/apache2ctl',
+    content => 'aegir ALL=(ALL) NOPASSWD: /usr/sbin/apache2ctl',
   }
 
   include profiles::apache
