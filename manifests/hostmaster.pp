8df5b933e8c83a4ef25ea273fa8bab4f7d35a08c
Tom Sneyers <tom.sneyers@vito.be>
2014-10-27 12:53:31 +0100

    readded aegir_mysql profile

--- manifests/hostmaster.pp
+++ manifests/hostmaster.pp
@@ -4,9 +4,7 @@ class roles::hostmaster inherits roles::server {
     content => 'aegir ALL=(ALL) NOPASSWD: ALL',
   }
 
-  class { '::profiles::aegir::mysql': }
   class { '::profiles::aegir':
-      require => Class['::profiles::aegir::mysql']
   }
 
 
