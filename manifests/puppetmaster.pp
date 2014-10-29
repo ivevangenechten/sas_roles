2af6a44524481ed4c9fb2c3a569e71844162d4cd
Tom Sneyers <tom.sneyers@vito.be>
2014-10-29 06:23:04 +0100

    add puppetdb

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,6 +1,7 @@
 class roles::puppetmaster inherits roles::server {
 
-  #include puppetdb
-  #include puppetdb::master::config
-
+# Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+# Configure the puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
 }
