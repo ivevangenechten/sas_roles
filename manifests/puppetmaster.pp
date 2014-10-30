5da320b85a044948d2502ca2207ed2de4447df19
Tom Sneyers <tom.sneyers@vito.be>
2014-10-30 08:55:47 +0100

    removed everything from puppetdb

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,13 +1,3 @@
 class roles::puppetmaster inherits roles::server {
 
-# Configure puppetdb and its underlying database
-  class { 'puppetdb': }
-
-# Configure the puppet master to use puppetdb
-#  class { 'puppetdb::master::config': }
-
-  package { 'ruby-puppetdb-foreman':
-    ensure => 'present',
-  }
-
 }
