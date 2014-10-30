40209f2186e796fc471b95c7276740c43a389863
Tom Sneyers <tom.sneyers@vito.be>
2014-10-30 09:04:46 +0100

    puppetdb

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,3 +1,10 @@
 class roles::puppetmaster inherits roles::server {
 
+# Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+
+  package { 'ruby-puppetdb-foreman':
+    ensure => present,
+  }
+
 }
